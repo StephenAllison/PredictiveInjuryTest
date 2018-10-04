4938b60b626d2f375e421c11ff8ea0bd161c9f02
Stephen Allison <stephen.allison.technology@gmail.com>
2018-10-04 12:36:04 -0400

    formatting

--- backend/models/Athlete-Models/athleteProfile.js
+++ backend/models/Athlete-Models/athleteProfile.js
@@ -2,11 +2,11 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const athleteProfileSchema = new Schema({
-  Sport: String,
-  League: String,
-  Team: String,
-  Name: String,
-  Position: String,
+  sport: String,
+  league: String,
+  team: String,
+  name: String,
+  position: String,
   moderators: { type: Schema.Types.ObjectId, ref: "athleteModerators" },
   mediators: { type: Schema.Types.ObjectId, ref: "athleteMediators" }
 });
