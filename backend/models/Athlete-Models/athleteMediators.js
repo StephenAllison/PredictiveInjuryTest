cbcd81da8b59d89307ee37e269e262762ada12a3
Stephen Allison <stephen.allison.technology@gmail.com>
2018-10-04 23:07:40 -0400

    Mediator Model Revised

--- backend/models/Athlete-Models/athleteMediators.js
+++ backend/models/Athlete-Models/athleteMediators.js
@@ -1,18 +1,18 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-//Athlete Model
+//Athlete Mediating Factors Model
 const athleteMediatorSchema = new Schema({
-  //Physical
-  previousInjuryHistory: Number,
-  fatigue: Number,
-  //Psychological
-  angerAggression: Number,
-  impulsiveness: Number,
-  burnout: Number,
-  //Social
-  organizationalPressure: Number,
-  socialPressure: Number
+  physicalMediatingFactorScore: Number,
+  // previousInjuryHistory: Number,
+  // fatigue: Number,
+  psychologicalMediatingFactors: Number,
+  // angerAggression: Number,
+  // impulsiveness: Number,
+  // burnout: Number,
+  socialMediatingFactorScore: Number
+  // organizationalPressure: Number,
+  // socialPressure: Number
 });
 
 const AthleteMediator = mongoose.model(
